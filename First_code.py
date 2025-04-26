f6f247ba99da7b2b084c0da2c2c47d11de7d9419
AlexAlgorithmist <am.popov.09@gamil.com>
2025-04-26 16:57:21 +0300

    fixed bug

--- First_code.py
+++ First_code.py
@@ -10,7 +10,7 @@ print(f'{tim2 + ale2 = }\n{tim2 - ale2 = }\n{tim2 / ale2 = }\n{tim2 * ale2 = }')
 import turtle as t
 from math import log
 
-for i in rnage(100):
+for i in range(100):
     t.forward(log(i))
     t.right(log(i))
 
